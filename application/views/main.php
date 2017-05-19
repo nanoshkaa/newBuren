4f10848d9e032e2b8f5b2c7bded9384ca3cc8bb0
Naima Daruich <nanoshkaa@gmail.com>
2017-05-19 16:31:30 +0200

    buttom link main

--- application/views/main.php
+++ application/views/main.php
@@ -10,76 +10,70 @@
       crossorigin="anonymous"></script>
   <meta charset="UTF-8">
   <title>Nieuwe Buren</title>
-  <meta name="viewport" content="width=device-width">
- 
-
-  
-      
-  
+  <meta name="viewport" content="width=device-width"> 
 </head>
 
-<body>
-<div class="first_row">
-  <div id="nav-trigger" class="nav-trigger open">
-    <span class="line"></span>
-    <span class="line"></span>
-    <span class="line"></span>
-  </div>
-   <div class=" navbar-collapse" >
-          <ul class="sign">
-            <li><a href="/users/show_sign_in">Sign In</a></li>
-	        <li><a href="/users/show_sign_up">Sign Up</a></li>
-	        <li><a href="/users/sign_out">Sign Out</a></li>
-	        <li><a href="/users/show_profile">Profile</a></li>
-	        <li><a href="/users/show_my_profile">My Profile</a></li>
-	        <li><a href="/users/faq">FAQ</a></li>
-	        <li><a href="/users/contact">Contact</a></li>
-          </ul>
-    </div><!-- /.navbar-collapse -->
-</div>
-<nav id="nav" class="out">
-  <ul>
-    <li><span class="nav-label">Testimonials</span></span></li>
-    <li><span class="nav-label">Slider Met Profielen</span></li>
-    <li><span class="nav-label">Events</span></li>
-    <li><span class="nav-label">Sponsors</span></li>
-    <li><span class="nav-label">Mensen Achter NB</span></li>
-  </ul>
-</nav>
-<section id="header" class="header">
-<img  src="15DINNERJP1-master675.jpg">
+	<body>
+	<div class="first_row">
+	  <div id="nav-trigger" class="nav-trigger open">
+	    <span class="line"></span>
+	    <span class="line"></span>
+	    <span class="line"></span>
+	  </div>
+	   <div class=" navbar-collapse" >
+	          <ul class="sign">
+	            <li><a href="/users/show_sign_in">Sign In</a></li>
+		        <li><a href="/users/show_sign_up">Sign Up</a></li>
+		        <li><a href="/users/sign_out">Sign Out</a></li>
+		        <li><a href="/users/show_my_profile">My Profile</a></li>
+		        <li><a href="/users/faq">FAQ</a></li>
+		        <li><a href="/users/contact">Contact</a></li>
+	          </ul>
+	    </div><!-- /.navbar-collapse -->
+	</div>
+	<nav id="nav" class="out">
+	  <ul>
+	    <li><span class="nav-label">Testimonials</span></span></li>
+	    <li><span class="nav-label">Slider Met Profielen</span></li>
+	    <li><span class="nav-label">Events</span></li>
+	    <li><span class="nav-label">Sponsors</span></li>
+	    <li><span class="nav-label">Mensen Achter NB</span></li>
+	  </ul>
+	</nav>
+	<section id="header" class="header">
+	<img  src="15DINNERJP1-master675.jpg">
 
-  <h1 id="heading">Multiculti dineren</h1>
-</section>
-  <section class="content">
-        <img src="Capture.png" alt="Capture" width="175">
-        <p>Begin met multiculti dineren door hieronder aan te geven of je graag te gast wilt zijn of dat je als host een diner wilt organiseren.</p>
-        <button type="button" class="btn btn-primary btn-lg">Bekijk gezinnen die al mee doen</button>
-  </section>
-  <div class="row">
-      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <div class="row_footer">
-          <footer class ="col-lg-12">
-           <div class="footer-social-icon">
-             <h2>Volg ons op Facebook</h2>
-              <ul class="social-icons">
-                <li><a target="_blank" href="https://nl-nl.facebook.com/nieuweburenrtl/" class="social-icon">
-                 <i class="fa fa-facebook"></i></a>
-               </li>
-             </ul>
-           </div>
-          </footer>
-         <div class="row_desinger">
-           <h4>Designed by:</h4>
-            <a href="https://www.linkedin.com/in/mohammad-abo-hassan-780462113/">Mohammad</a><br>
-            <a href="https://www.linkedin.com/in/naima-daruich-b64128b/">Naima</a><br>
-            <a href="https://www.linkedin.com/in/hameed-alhowidi-a24b58117/">Hmeed</a><br>
-         </div>
-        </div>
-      </div>
-  </div>
- 
-  <script src="/assets/javascript/index.js"></script>
+	  <h1 id="heading">Multiculti dineren</h1>
+	</section>
+	  <section class="content">
+	        <img src="Capture.png" alt="Capture" width="175">
+	        <p>Begin met multiculti dineren door hieronder aan te geven of je graag te gast wilt zijn of dat je als host een diner wilt organiseren.</p>
+	        <a href="/users/show_profile"> <button type="button" class="btn btn-primary btn-lg">Bekijk gezinnen die al mee doen</button></a>
+	  </section>
+	  <div class="row">
+	      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	        <div class="row_footer">
+	          <footer class ="col-lg-12">
+	           <div class="footer-social-icon">
+	             <h2>Volg ons op Facebook</h2>
+	              <ul class="social-icons">
+	                <li><a target="_blank" href="https://nl-nl.facebook.com/nieuweburenrtl/" class="social-icon">
+	                 <i class="fa fa-facebook"></i></a>
+	               </li>
+	             </ul>
+	           </div>
+	          </footer>
+	         <div class="row_desinger">
+	           <h4>Designed by:</h4>
+	            <a href="https://www.linkedin.com/in/mohammad-abo-hassan-780462113/">Mohammad</a><br>
+	            <a href="https://www.linkedin.com/in/naima-daruich-b64128b/">Naima</a><br>
+	            <a href="https://www.linkedin.com/in/hameed-alhowidi-a24b58117/">Hmeed</a><br>
+	         </div>
+	        </div>
+	      </div>
+	  </div>
+	 
+	  <script src="/assets/javascript/index.js"></script>
 
-</body>
+	</body>
 </html>
