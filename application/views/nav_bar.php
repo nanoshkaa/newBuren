fbe2fd383b9116d76162cd41e923b0e17a53ef1d
Ahalhowidi <ah.alhowidi@gmail.com>
2017-05-22 18:37:29 +0200

    nav bar

--- application/views/nav_bar.php
+++ application/views/nav_bar.php
@@ -2,7 +2,6 @@
 	          <ul class="nav nav-tabs nav-justified">
 	        	<li><a href="/users/index">Main</a></li>
 	        	<li><a href="/users/show_profile">Profiles</a></li>
-		        <li><a href="/users/show_my_profile">My Profile</a></li>
 		        <li><a href="/users/faq">Events</a></li>
 		        <li><a href="/users/faq">FAQ</a></li>
 		        <li><a href="/users/contact">Contact</a></li>
@@ -10,6 +9,7 @@
 		        <li><a href="/users/show_sign_in">Sign In</a></li>
 		        <li><a href="/users/show_sign_up">Sign Up</a></li>
 		        		        <?php }	else{ ?>
+		        <li><a href="/users/show_my_profile">My Profile</a></li>
 		        <li><a href="/users/sign_out">Sign Out</a></li>
    <?php } ?>
 	          </ul> 
