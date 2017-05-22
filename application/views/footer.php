77d820c407bdd8f7b0e48a9d105aae4f5c12fde7
Naima Daruich <nanoshkaa@gmail.com>
2017-05-22 11:16:51 +0200

    commit

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,7 @@
-   
- <div class="footer">
-  <center><i id="footer_text">Designed by Mohammed Naima Hameed</i></center>
-  
-  
+<div class="row_desinger">
+	<h5>Designed by:</h5>
+	<a href="https://www.linkedin.com/in/mohammad-abo-hassan-780462113/">Mohammad</a>
+	<a href="https://www.linkedin.com/in/naima-daruich-b64128b/">Naima</a>
+	<a href="https://www.linkedin.com/in/hameed-alhowidi-a24b58117/">Hameed</a>
+</div>
+
