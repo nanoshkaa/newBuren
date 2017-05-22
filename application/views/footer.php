f39003c2fa0936fe7790a39b7e0dfba12dca5c89
Naima Daruich <nanoshkaa@gmail.com>
2017-05-22 11:30:33 +0200

    footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,7 @@
 <div class="row_desinger">
-	<h5>Designed by:</h5>
-	<a href="https://www.linkedin.com/in/mohammad-abo-hassan-780462113/">Mohammad</a>
-	<a href="https://www.linkedin.com/in/naima-daruich-b64128b/">Naima</a>
-	<a href="https://www.linkedin.com/in/hameed-alhowidi-a24b58117/">Hameed</a>
+	<p>Designed by:
+	<a href="https://www.linkedin.com/in/mohammad-abo-hassan-780462113/">Mohammad,</a>
+	<a href="https://www.linkedin.com/in/naima-daruich-b64128b/">Naima,</a>
+	<a href="https://www.linkedin.com/in/hameed-alhowidi-a24b58117/">Hameed</a></p>
 </div>
 
--- application/views/main.php
+++ application/views/main.php
@@ -31,40 +31,20 @@
 	  </ul>
 	</nav>
 	<section id="header" class="header">
-
-	<img  src="../assets/image/main_pic/15DINNERJP1-master675.jpg">
-
-	  <h1 id="heading">Multiculti dineren</h1>
+		<img  src="../assets/image/main_pic/15DINNERJP1-master675.jpg">
+		<h1 id="heading">Multiculti dineren</h1>
 	</section>
-	  <section class="content">
-	        <img src="../assets/image/main_pic/Capture.png" alt="Capture" width="175">
-	        <p>Begin met multiculti dineren door hieronder aan te geven of je graag te gast wilt zijn of dat je als host een diner wilt organiseren.</p>
-	        <a href="/users/show_profile"> <button type="button" class="btn btn-primary btn-lg">Bekijk gezinnen die al mee doen</button></a>
-	  </section>
-	  <div class="row">
-	      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-	        <div class="row_footer">
-	          <footer class ="col-lg-12">
-	           <div class="footer-social-icon">
-	             <h2>Volg ons op Facebook</h2>
-	              <ul class="social-icons">
-	                <li><a target="_blank" href="https://nl-nl.facebook.com/nieuweburenrtl/" class="social-icon">
-	                 <i class="fa fa-facebook"></i></a>
-	               </li>
-	             </ul>
-	           </div>
-	          </footer>
-	         <div class="row_desinger">
-	           <h4>Designed by:</h4>
-	            <a href="https://www.linkedin.com/in/mohammad-abo-hassan-780462113/">Mohammad</a><br>
-	            <a href="https://www.linkedin.com/in/naima-daruich-b64128b/">Naima</a><br>
-	            <a href="https://www.linkedin.com/in/hameed-alhowidi-a24b58117/">Hameed</a><br>
-	         </div>
-	        </div>
-	      </div>
-	  </div>
-	 
-	  <script src="/assets/javascript/index.js"></script>
-
+	<section class="content">
+        <img src="../assets/image/main_pic/Capture.png" alt="NieuweBurenLogo" width="175">
+        <p>Begin met multiculti dineren door hieronder aan te geven of je graag te gast wilt zijn of dat je als host een diner wilt organiseren.</p>
+        <a href="/users/show_profile"> <button type="button" class="btn btn-primary btn-lg">Bekijk gezinnen die al mee doen</button></a>
+	</section>
+	<div class="row_footer col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h2>Volg ons op Facebook</h2>
+        <a target="_blank" href="#" class="social-icon">
+        <i class="fa fa-facebook"></i></a>
+        <?php $this->load->view('footer'); ?>
+	</div>
+	<script src="/assets/javascript/index.js"></script>
 	</body>
 </html>
