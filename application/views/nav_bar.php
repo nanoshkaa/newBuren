12241fb521d90aa8b4fbfd83a93d54888eef8b2f
Ahalhowidi <ah.alhowidi@gmail.com>
2017-05-22 11:38:59 +0200

    nav bar

--- application/views/nav_bar.php
+++ application/views/nav_bar.php
@@ -4,15 +4,15 @@
 	          <ul class="nav nav-tabs nav-justified">
 	        	<li><a href="/users/index">Main</a></li>
 		        <li><a href="/users/show_my_profile">My Profile</a></li>
-		        <li><a href="/users/faq">Testimonials</a></li>
 		        <li><a href="/users/faq">Events</a></li>
-		        <li><a href="/users/faq">Sponsors</a></li>
-		        <li><a href="/users/faq">Mensen Achter NB</a></li>
 		        <li><a href="/users/faq">FAQ</a></li>
 		        <li><a href="/users/contact">Contact</a></li>
+		        <?php if (empty($this->session->userdata('logged_in'))) { ?>
 		        <li><a href="/users/show_sign_in">Sign In</a></li>
 		        <li><a href="/users/show_sign_up">Sign Up</a></li>
+		        		        <?php }	else{ ?>
 		        <li><a href="/users/sign_out">Sign Out</a></li>
+   <?php } ?>
 	          </ul>
 	    </div> 
 	</div>
