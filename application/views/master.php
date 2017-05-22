036abd194006ee5bb6e5b339e3fe8274ff6327c5
Naima Daruich <nanoshkaa@gmail.com>
2017-05-22 20:50:26 +0200

    some styling

--- application/views/master.php
+++ application/views/master.php
@@ -27,6 +27,79 @@
     </script>
     <style>
     @import url(http://fonts.googleapis.com/css?family=Oswald:400,700,300);
+    @import url(http://fonts.googleapis.com/css?family=Open+Sans:300,800);
+body {
+  font-size: 1.5em;
+  background-color: white;
+}
+.content {
+  width: 80%;
+  padding: 20px;
+  margin: 0 auto;
+  padding: 0 60px 0 0;
+}
+.centerplease {
+  margin: 0 auto;
+  max-width: auto;
+  font-size: 40px;
+  color: #f3c59f;
+}
+.question {
+  position: relative;
+  background: #f3c59f;
+  margin: 0;
+  padding: 10px 10px 10px 50px;
+  display: block;
+  width:100%;
+  cursor: pointer;
+  color: white;
+}
+.answers {
+  background: white;
+  padding: 0px 15px;
+  margin: 5px 0;
+  height: 0;
+  overflow: hidden;
+  z-index: -1;
+  position: relative;
+  opacity: 0;
+  -webkit-transition: .7s ease;
+  -moz-transition: .7s ease;
+  -o-transition: .7s ease;
+  transition: .7s ease;
+}
+.questions:checked ~ .answers{
+  height: auto;
+  opacity: 1;
+  padding: 15px;
+}
+.plus {
+  position: absolute;
+  margin-left: 10px;
+  margin-top: 8px;
+  z-index: 5;
+  font-size: 2em;
+  line-height: 100%;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -o-user-select: none;
+  user-select: none;
+  -webkit-transition: .3s ease;
+  -moz-transition: .3s ease;
+  -o-transition: .3s ease;
+  transition: .3s ease;
+  color: white;
+}
+.questions:checked ~ .plus {
+  -webkit-transform: rotate(45deg);
+  -moz-transform: rotate(45deg);
+  -o-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+.questions {
+  display: none;
+}
 .centered {
   margin: 0 auto;
   text-align: center;
@@ -142,5 +215,8 @@ button#send {
   padding: 40px;
   padding-top: 55px;
   height: auto;
+}
+.nav-stacked{
+  margin: 0px auto;
 }
     </style>
