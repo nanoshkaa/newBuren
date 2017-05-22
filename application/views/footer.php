1982e06ef97b58a21266327c6318a085d8066c47
Naima Daruich <nanoshkaa@gmail.com>
2017-05-22 11:38:49 +0200

    center

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,7 +1,7 @@
-<div class="row_desinger">
+<footer class="navbar-fixed-bottom container text-center">
 	<p>Designed by:
 	<a href="https://www.linkedin.com/in/mohammad-abo-hassan-780462113/">Mohammad,</a>
 	<a href="https://www.linkedin.com/in/naima-daruich-b64128b/">Naima,</a>
 	<a href="https://www.linkedin.com/in/hameed-alhowidi-a24b58117/">Hameed</a></p>
-</div>
+</footer>
 
