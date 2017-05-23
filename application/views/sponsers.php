eb361aa078458f7ece3bbbb91eed1b0475263431
Naima Daruich <nanoshkaa@gmail.com>
2017-05-23 21:35:19 +0200

    more style with sponsers

---
+++ application/views/sponsers.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sponsers</title>
+	<?php $this->load->view('master'); ?>
+</head>
+<body>
+	<?php $this->load->view('nav_bar'); ?>
+	<div class="container">
+		<div>
+            <img class="sponsers_logo  img-responsive" src="../assets/image/sponsers/1.png" alt="logo"><br>
+            <p>Unilever</p><br>
+        </div>
+        <div>
+            <img class="sponsers_logo img-responsive" src="../assets/image/sponsers/8.png" alt="logo"><br>
+            <p>Vluchtelingen werk Nederland</p><br>
+        </div>
+        <div>
+            <img class="sponsers_logo img-responsive" src="../assets/image/sponsers/3.png" alt="logo"><br>
+            <p>Hoedje van Papier</p><br>
+        </div>
+        <div>
+            <img class="sponsers_logo  img-responsive" src="../assets/image/sponsers/4.png" alt="logo"><br>
+            <p>Restart Network</p><br>
+        </div>
+        <div>
+            <img class="sponsers_logo  img-responsive" src="../assets/image/sponsers/5.png" alt="logo"><br>
+            <p>Nysingh</p><br>
+        </div> 
+        <div>
+            <img class="sponsers_logo  img-responsive" src="../assets/image/sponsers/6.png" alt="logo"><br>
+            <p>Blik op Werk</p><br>
+        </div>
+        <div>
+            <img class="sponsers_logo  img-responsive" src="../assets/image/sponsers/7.png" alt="logo"><br>
+            <p>Gemeente Rotterdam</p><br>
+        </div>
+	</div>
+	<?php $this->load->view('footer'); ?>
+</body>
+</html>
