d4ec6dec158ea150327fb01fac4a5e256f6a0f11
Naima Daruich <nanoshkaa@gmail.com>
2017-05-23 22:20:42 +0200

    update

--- application/views/events.php
+++ application/views/events.php
@@ -7,14 +7,13 @@
 <body>
 	<?php $this->load->view('nav_bar'); ?>
     <div class="chat">
-    	<h2>Nieuwe Buren</h2>
     	<img src="../assets/image/main_pic/Capture.png" alt="NieuweBurenLogo" width="175">
         <p>Begin met multiculti dineren door hieronder aan te geven of je graag te gast wilt zijn of dat je als host een diner wilt organiseren.</p>
-        <a target="_blank" href="https://www.facebook.com/events/114801509073951/?acontext=%7B%22ref%22%3A%223%22%2C%22ref_newsfeed_story_type%22%3A%22regular%22%2C%22feed_story_type%22%3A%2217%22%2C%22action_history%22%3A%22null%22%7D" class="social-icon"><h2>Inschrijven op Facebook</h2>
+        <a target="_blank" href="https://www.facebook.com/events/114801509073951/?acontext=%7B%22ref%22%3A%223%22%2C%22ref_newsfeed_story_type%22%3A%22regular%22%2C%22feed_story_type%22%3A%2217%22%2C%22action_history%22%3A%22null%22%7D" class="social-icon"><h2>Inschrijf op Facebook</h2>
         </a>
     </div>
     <section>
-        <img src="../assets/image/events/n.jpg" alt="NieuweBurenEvent">
+        <img src="../assets/image/events/1.png" alt="NieuweBurenEvent">
     </section>
     <div>
         
